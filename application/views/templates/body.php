dcae3fd04a4faa72fe7e1a7704081f725e783c4a
it3r4-gonzales <45382611+it3r4-gonzales@users.noreply.github.com>
2019-03-24 15:07:51 +0800

    revision

--- application/views/templates/body.php
+++ application/views/templates/body.php
@@ -1,4 +1,4 @@
 <div class="">
 	<h1><center><b>WELCOME TO MY GITAPP</b></center></h1>
-        <p> Gonzales edited this in sunday afternoon</p>
+        <p> Gonzales edited this on sunday afternoon</p>
 </div>
